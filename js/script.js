1efd6912c69063617d8c03677354c7a7a771b264
archturtle <siddhartha.juluru@gmail.com>
2020-01-04 15:19:16 -0500

    Added JSON preview

--- js/script.js
+++ js/script.js
@@ -1,158 +1,159 @@
 class Generator {
-
     get genData() {
         return this._genData
     }
+
     set genData(value) {
         this._genData = value
     }
-
+    
     constructor() {
-        this.genData = {}
-        this.prepareQuerySel();
+        this.genData = {};
+        this.prepareEvents();
         this.clearInputs();
     }
 
-    prepareQuerySel() {
+    prepareEvents() {
         document.querySelectorAll('.typeSelect#generator .selectItem').forEach(el => {
-
             el.addEventListener('click', () => {
-                if (!el.classList.contains('selected')) {
-                    this.switchGenerator(el)
-                }
+                if (!el.classList.contains('selected')) this.switchGenerator(el);
             });
 
             el.addEventListener('touchstart', () => {
-                if (!el.classList.contains('selected')) {
-                    this.switchGenerator(el)
-                }
-            })
-        })
+                if (!el.classList.contains('selected')) this.switchGenerator(el);
+            });
+        });
 
         document.querySelectorAll('.typeSelect#theme .selectItem').forEach(el => {
-
             el.addEventListener('click', () => {
-                if (!el.classList.contains('selected')) {
-                    this.switchTheme(el)
-                }
-            })
+                if (!el.classList.contains('selected')) this.switchTheme(el);
+            });
 
             el.addEventListener('touchstart', () => {
-                if (!el.classList.contains('selected')) {
-                    this.switchTheme(el)
-                }
-            })
-        })
+                if (!el.classList.contains('selected')) this.switchTheme(el);
+            });
+        });
 
         document.querySelectorAll('.generator .entry input, .generator .entry textarea').forEach(el => {
-            el.addEventListener('keyup', () => {
-                this.updateGenData(el)
-            })
-        })
+            el.addEventListener('keyup', () => { this.updateGenData(el) });
+        });
 
         document.querySelectorAll('.generator .entry select').forEach(el => {
+            el.addEventListener('change', () => { this.updateGenData(el) });
+        });
 
-            el.addEventListener('change', () => {
-                this.updateGenData(el)
-            })
-        })
-
-        document.querySelector('a.download').addEventListener('click', () => {
-            this.clearInputs()
+        document.querySelector("div.preview").addEventListener("click", () => {
+            if (!document.querySelector("div.preview").classList.contains("expanded")) {
+                document.querySelector("div.preview").classList.add("expanded");
+                document.querySelector("div.preview > div.json").classList.remove("hidden");
+                document.querySelector("div.preview > a.download").classList.remove("hidden");
+            } else {
+                document.querySelector("div.preview").classList.remove("expanded");
+                document.querySelector("div.preview > div.json").classList.add("hidden");
+                document.querySelector("div.preview > a.download").classList.add("hidden");
+            }
         })
     }
 
-    switchGenerator(element) {
-        let type = element.getAttribute('data-type')
-        let parent = element.parentElement
-        if (type === 'plugins') {
-            parent.querySelector('.selectItem[data-type="themes"]').classList.remove('selected')
-            parent.querySelector('.selectItem[data-type="plugins"]').classList.add('selected')
+    switchTheme(element) {
+        const type = element.getAttribute('data-type');
+        const parent = element.parentElement;
+        const highlightCssLink = document.getElementsByTagName("link").item(1);
 
-            document.querySelector('.generator[data-type="themes"]').classList.remove('selected')
-            document.querySelector('.generator[data-type="plugins"]').classList.add('selected')
+        if (type === 'light') {
+            parent.querySelector('.selectItem[data-type="dark"]').classList.remove('selected');
+            parent.querySelector('.selectItem[data-type="light"]').classList.add('selected');
+            
+            const newHighlightCssLink = document.createElement("link");
+            newHighlightCssLink.setAttribute("rel", "stylesheet");
+            newHighlightCssLink.setAttribute("type", "text/css");
+            newHighlightCssLink.setAttribute("href", "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.17.1/build/styles/atom-one-light.min.css");
+
+            document.getElementsByTagName("head").item(0).replaceChild(newHighlightCssLink, highlightCssLink);
+            
+            document.body.classList.add('light');
+            document.body.classList.remove('dark');
         } else {
-            parent.querySelector('.selectItem[data-type="plugins"]').classList.remove('selected')
-            parent.querySelector('.selectItem[data-type="themes"]').classList.add('selected')
+            parent.querySelector('.selectItem[data-type="light"]').classList.remove('selected');
+            parent.querySelector('.selectItem[data-type="dark"]').classList.add('selected');
+            
+            const newHighlightCssLink = document.createElement("link");
+            newHighlightCssLink.setAttribute("rel", "stylesheet");
+            newHighlightCssLink.setAttribute("type", "text/css");
+            newHighlightCssLink.setAttribute("href", "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.17.1/build/styles/atom-one-dark.min.css");
 
-            document.querySelector('.generator[data-type="plugins"]').classList.remove('selected')
-            document.querySelector('.generator[data-type="themes"]').classList.add('selected')
+            document.getElementsByTagName("head").item(0).replaceChild(newHighlightCssLink, highlightCssLink);
+
+            document.body.classList.add('dark');
+            document.body.classList.remove('light');
         }
-        document.querySelector('a.download').classList.add('disabled')
+    }
+
+    switchGenerator(element) {
+        const type = element.getAttribute('data-type')
+        const parent = element.parentElement
+
+        if (type === 'plugins') {
+            parent.querySelector('.selectItem[data-type="themes"]').classList.remove('selected');
+            parent.querySelector('.selectItem[data-type="plugins"]').classList.add('selected');
+
+            document.querySelector('.generator[data-type="themes"]').classList.remove('selected');
+            document.querySelector('.generator[data-type="plugins"]').classList.add('selected');
+        } else {
+            parent.querySelector('.selectItem[data-type="plugins"]').classList.remove('selected');
+            parent.querySelector('.selectItem[data-type="themes"]').classList.add('selected');
+
+            document.querySelector('.generator[data-type="plugins"]').classList.remove('selected');
+            document.querySelector('.generator[data-type="themes"]').classList.add('selected');
+        }
+
+        document.querySelector("div.preview").classList.add('disabled')
         this.genData = {}
         this.clearInputs()
     }
 
-    switchTheme(element) {
-        let type = element.getAttribute('data-type')
-        let parent = element.parentElement
-
-        if (type === 'dark') {
-            parent.querySelector('.selectItem[data-type="light"]').classList.remove('selected')
-            parent.querySelector('.selectItem[data-type="dark"]').classList.add('selected')
-
-            document.body.classList.add('dark')
-            document.body.classList.remove('light')
-        } else {
-            parent.querySelector('.selectItem[data-type="dark"]').classList.remove('selected')
-            parent.querySelector('.selectItem[data-type="light"]').classList.add('selected')
-
-            document.body.classList.add('light')
-            document.body.classList.remove('dark')
-        }
-    }
-
-    /**
-    *
-    * @param {HTMLElement} element
-    */
     updateGenData(element) {
-
-        let inputType = element.id.slice(10, element.id.length)
+        const inputType = element.id.slice(10, element.id.length);
 
         if (element.value !== '') {
-            this.genData[inputType] = element.value
-            element.closest('.entry').setAttribute('data-filled', "true")
-        }
-        else {
-            delete this.genData[inputType]
-            element.closest('.entry').setAttribute('data-filled', "false")
+            this.genData[inputType] = element.value;
+            element.closest('.entry').setAttribute('data-filled', "true");
+        } else {
+            delete this.genData[inputType];
+            element.closest('.entry').setAttribute('data-filled', "false");
         }
 
-        this.checkIfDataRequired()
-
+        this.checkIfDataRequired();
     }
 
     checkIfDataRequired() {
-
-        let requiredFilled = true
+        let requiredFilled = true;
 
         document.querySelectorAll('.selected .entry[data-required=true]').forEach(el => {
-
             if (requiredFilled) {
                 if (el.getAttribute('data-filled') === "false") {
-                    requiredFilled = false
+                    requiredFilled = false;
                 }
-            }
+             }
+        });
 
-        })
+        document.querySelector("div.preview > div.json > pre > code").innerHTML = JSON.stringify(this.genData, null, 4);
+        hljs.highlightBlock(document.querySelector("div.preview > div.json > pre > code"));
+
+        document.styleSheets[2].cssRules[37].style.height = `${((Object.keys(this.genData).length + 2) * 28) + (12 * 2) + (19.5 * 2) + (29 * 2)}px`
 
         if (requiredFilled) {
-            this.generateDownloadFile()
-            document.querySelector('a.download').classList.remove('disabled')
+            this.downloadManifestFile();
+            document.querySelector("div.preview").classList.remove('disabled');
         } else {
-            document.querySelector('a.download').classList.add('disabled')
+            document.querySelector("div.preview").classList.add('disabled');
         }
-
     }
 
-    generateDownloadFile() {
-
+    downloadManifestFile() {
         const blob = new Blob([JSON.stringify(this.genData, null, 4)], { type: 'application/json' });
-        const dlLink = document.querySelector('a.download')
-        dlLink.href = URL.createObjectURL(blob)
-        dlLink.download = document.querySelector('.generator.selected').getAttribute('data-fileName')
-
+        document.querySelector("div.preview > a.download").href = URL.createObjectURL(blob);
+        document.querySelector("div.preview > a.download").download = document.querySelector('.generator.selected').getAttribute('data-fileName');
     }
 
     clearInputs() {
